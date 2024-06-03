d4b926c1891913c952ce085fca35ba49cbc87fd7
Atlzf <1924855952@qq.com>
2024-06-04 01:10:47 +0800

    更改

--- src/java/com/bitzh/hospitalsystem/dao/ReviewDao.java
+++ src/java/com/bitzh/hospitalsystem/dao/ReviewDao.java
@@ -1,5 +1,61 @@
 package java.com.bitzh.hospitalsystem.dao;
 
-//包含了与评论表进行交互的方法，如添加评论、删除评论、修改评论信息和查看评论列表
+import java.com.bitzh.hospitalsystem.Utils.DatabaseConnectionManager;
+import java.com.bitzh.hospitalsystem.model.Review;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReviewDao {
+    private Connection conn;
+
+    public ReviewDao(Connection conn) throws SQLException {
+        this.conn = DatabaseConnectionManager.getConnection();
+    }
+
+    public void addReview(Review review) throws SQLException {
+        String sql = "INSERT INTO Review (user_id, doctor_id, content) VALUES (?, ?, ?)";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setInt(1, review.getUserId());
+        pstmt.setInt(2, review.getDoctorId());
+        pstmt.setString(3, review.getContent());
+        pstmt.executeUpdate();
+    }
+
+    public List<Review> getReviewsByUserId(int userId) throws SQLException {
+        List<Review> reviews = new ArrayList<>();
+        String sql = "SELECT * FROM Review WHERE user_id = ?";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setInt(1, userId);
+        ResultSet rs = pstmt.executeQuery();
+        while (rs.next()) {
+            Review review = new Review();
+            review.setId(rs.getInt("id"));
+            review.setUserId(rs.getInt("user_id"));
+            review.setDoctorId(rs.getInt("doctor_id"));
+            review.setContent(rs.getString("content"));
+            reviews.add(review);
+        }
+        return reviews;
+    }
+
+    public List<Review> getReviewsByDoctorId(int doctorId) throws SQLException {
+        List<Review> reviews = new ArrayList<>();
+        String sql = "SELECT * FROM Review WHERE doctor_id = ?";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setInt(1, doctorId);
+        ResultSet rs = pstmt.executeQuery();
+        while (rs.next()) {
+            Review review = new Review();
+            review.setId(rs.getInt("id"));
+            review.setUserId(rs.getInt("user_id"));
+            review.setDoctorId(rs.getInt("doctor_id"));
+            review.setContent(rs.getString("content"));
+            reviews.add(review);
+        }
+        return reviews;
+    }
 }
--- src/java/com/bitzh/hospitalsystem/model/Review.java
+++ src/java/com/bitzh/hospitalsystem/model/Review.java
@@ -1,5 +1,41 @@
 package java.com.bitzh.hospitalsystem.model;
 
-//  包含评价的信息，如用户ID、医生ID和评价内容
 public class Review {
+    private int id;
+    private int userId;
+    private int doctorId;
+    private String content;
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(int doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
