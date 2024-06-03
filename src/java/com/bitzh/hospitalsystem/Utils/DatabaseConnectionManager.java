ce72faad06e07377a938d118603eca26ac364c5c
Atlzf <1924855952@qq.com>
2024-06-04 01:12:18 +0800

    更改

--- src/java/com/bitzh/hospitalsystem/Utils/DatabaseConnectionManager.java
+++ src/java/com/bitzh/hospitalsystem/Utils/DatabaseConnectionManager.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DatabaseConnectionManager {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/HospitalSystem";
-    private static final String USER = "admin";
+    private static final String USER = "root";
     private static final String PASS = "flan0204";
 
     public static Connection getConnection() throws SQLException {
