5d0d1309459f53e64fc84c9b70e69b40b6cbf10f
Atlzf <1924855952@qq.com>
2024-06-04 00:05:38 +0800

    更改

--- src/java/com/bitzh/hospitalsystem/dao/DoctorDao.java
+++ src/java/com/bitzh/hospitalsystem/dao/DoctorDao.java
@@ -1,5 +1,30 @@
 package java.com.bitzh.hospitalsystem.dao;
 
+import java.sql.*;
+import java.com.bitzh.hospitalsystem.model.*;
+
+
 //包含了与医生表进行交互的方法，如添加医生、删除医生、修改医生信息、查看医生列表和查看医生信息
 public class DoctorDao {
+    private Connection conn;
+    // 构造函数，用于建立数据库连接
+    public DoctorDao(Connection conn) {
+        this.conn = conn;
+    }
+    public Doctor Login(String username,String password)throws SQLException {
+        String sql = "select * from Doctor where username = ? and password = ?";
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.setString(1, username);//将第一个问好填充为username，防止注入
+        stmt.setString(2, password);
+        ResultSet rs = stmt.executeQuery();
+        if(rs.next()){
+            Doctor doctor = new Doctor();
+            doctor.setUsername(rs.getString("username"));
+            doctor.setPassword(rs.getString("password"));
+            return doctor;
+        }else{
+            return null;
+        }
+    }
+
 }
--- src/java/com/bitzh/hospitalsystem/model/Doctor.java
+++ src/java/com/bitzh/hospitalsystem/model/Doctor.java
@@ -2,4 +2,72 @@ package java.com.bitzh.hospitalsystem.model;
 
 //  包含医生的信息，如姓名、专长和可用时间
 public class Doctor {
+    private int id;
+    private String name;
+    private String specialty;
+    private String available_time;
+    private String username;
+    private String password;
+
+    // 无参数构造函数
+    public Doctor() {
+    }
+
+    public Doctor(int id, String name, String specialty, String available_time, String username, String password) {
+        this.id = id;
+        this.name = name;
+        this.specialty = specialty;
+        this.available_time = available_time;
+        this.username = username;
+        this.password = password;
+    }
+    // getter 和 setter 方法
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+    public String getAvailable_time() {
+        return available_time;
+    }
+
+    public void setAvailable_time(String available_time) {
+        this.available_time = available_time;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
