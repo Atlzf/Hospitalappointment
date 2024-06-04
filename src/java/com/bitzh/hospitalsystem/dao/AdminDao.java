fb4ea44f4fdca690ab5ae641f6c80e19728a1738
Atlzf <1924855952@qq.com>
2024-06-04 22:09:35 +0800

    更改

--- src/java/com/bitzh/hospitalsystem/dao/AdminDao.java
+++ src/java/com/bitzh/hospitalsystem/dao/AdminDao.java
@@ -1,5 +1,74 @@
 package java.com.bitzh.hospitalsystem.dao;
 
-//包含了与管理员表进行交互的方法，如添加管理员、删除管理员、修改管理员信息和查看管理员列表
+import java.com.bitzh.hospitalsystem.Utils.DatabaseConnectionManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class AdminDao {
+    private Connection conn;
+
+    public AdminDao(Connection conn) throws SQLException {
+        this.conn = DatabaseConnectionManager.getConnection();
+    }
+
+    public boolean login(String username, String password) throws SQLException {
+        String sql = "SELECT * FROM Admin WHERE username = ? AND password = ?";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, username);
+        pstmt.setString(2, password);
+        ResultSet rs = pstmt.executeQuery();
+        return rs.next();
+    }
+    public void addDoctor(String name, String specialty, String available_time, String username, String password) throws SQLException {
+        String sql = "INSERT INTO Doctor (name, specialty, available_time, username, password) VALUES (?, ?, ?, ?, ?)";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, name);
+        pstmt.setString(2, specialty);
+        pstmt.setString(3, available_time);
+        pstmt.setString(4, username);
+        pstmt.setString(5, password);
+        pstmt.executeUpdate();
+    }
+
+    public void updateDoctor(int id, String name, String specialty, String available_time) throws SQLException {
+        String sql = "UPDATE Doctor SET name = ?, specialty = ?, available_time = ? WHERE id = ?";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, name);
+        pstmt.setString(2, specialty);
+        pstmt.setString(3, available_time);
+        pstmt.setInt(4, id);
+        pstmt.executeUpdate();
+    }
+
+    public void resetDoctorPassword(int id) throws SQLException {
+        String sql = "UPDATE Doctor SET password = ? WHERE id = ?";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, "123456");
+        pstmt.setInt(2, id);
+        pstmt.executeUpdate();
+    }
+
+    public void resetUserPassword(int id) throws SQLException {
+        String sql = "UPDATE User SET password = ? WHERE id = ?";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, "123456");
+        pstmt.setInt(2, id);
+        pstmt.executeUpdate();
+    }
+
+    public void deleteReview(int id) throws SQLException {
+        String sql = "DELETE FROM Review WHERE id = ?";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setInt(1, id);
+        pstmt.executeUpdate();
+    }
+
+    public void deleteUser(int id) throws SQLException {
+        String sql = "DELETE FROM User WHERE id = ?";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setInt(1, id);
+        pstmt.executeUpdate();
+    }
 }
--- src/java/com/bitzh/hospitalsystem/dao/AppointmentDao.java
+++ src/java/com/bitzh/hospitalsystem/dao/AppointmentDao.java
@@ -1,5 +1,56 @@
 package java.com.bitzh.hospitalsystem.dao;
 
+import java.com.bitzh.hospitalsystem.model.Appointment;
+import java.sql.*;
+import java.util.*;
+import java.com.bitzh.hospitalsystem.Utils.DatabaseConnectionManager;
+
 //这个类包含了与预约表进行交互的方法，如添加预约、删除预约、修改预约信息和查看预约列表。
 public class AppointmentDao {
+    private Connection conn;
+    public AppointmentDao(Connection conn) throws SQLException {
+        this.conn = DatabaseConnectionManager.getConnection();
+    }
+    ;
+
+
+    public void addAppointment(Appointment appointment) throws SQLException {
+        String sql = "INSERT INTO Appointment (user_id, doctor_id, appointment_time) VALUES (?, ?, ?)";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setInt(1, appointment.getUserId());
+        pstmt.setInt(2, appointment.getDoctorId());
+        pstmt.setTimestamp(3, appointment.getAppointmentTime());
+        pstmt.executeUpdate();
+    }
+    public void deleteAppointment(int id) throws SQLException {
+        String sql = "DELETE FROM Appointment WHERE id = ?";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setInt(1, id);
+        pstmt.executeUpdate();
+    }
+    public void updateAppointment(Appointment appointment) throws SQLException {
+        String sql = "UPDATE Appointment SET user_id = ?, doctor_id = ?, appointment_time = ? WHERE id = ?";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setInt(1, appointment.getUserId());
+        pstmt.setInt(2, appointment.getDoctorId());
+        pstmt.setTimestamp(3, appointment.getAppointmentTime());
+        pstmt.setInt(4, appointment.getId());
+        pstmt.executeUpdate();
+    }
+
+    public List<Appointment> getAppointments() throws SQLException {
+        List<Appointment> appointments = new ArrayList<>();
+        String sql = "SELECT * FROM Appointment";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        ResultSet rs = pstmt.executeQuery();
+        while (rs.next()) {
+            Appointment appointment = new Appointment();
+            appointment.setId(rs.getInt("id"));
+            appointment.setUserId(rs.getInt("user_id"));
+            appointment.setDoctorId(rs.getInt("doctor_id"));
+            appointment.setAppointmentTime(rs.getTimestamp("appointment_time"));appointments.add(appointment);
+            }
+        return appointments;
+    }
+
 }
